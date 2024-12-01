d3e1b5607b03a46c219db3fb64f3317a16f841c9
MD BONI AMIN JAYED <106344301+MDboni@users.noreply.github.com>
2024-12-01 16:07:17 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// document.getElementById('submit').addEventListener('click', () => {
+
+//    let ab = document.getElementById("onn") ;
+//    ab.innerHTML = ab.innerHTML === "ON"?"OF":"ON";
+
+// })
+
+
